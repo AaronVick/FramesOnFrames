479ecae2b33cb09d38bf7391039cae4d9b9d33d6
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-09-24 13:59:51 -0400

    update

--- pages/api/frame.js
+++ pages/api/frame.js
@@ -10,13 +10,16 @@ export default async function handler(req, res) {
   let frameIndex = parseInt(untrustedData?.state || '-1');
 
   if (buttonIndex === 1) {
+    // Previous button pressed
     frameIndex = frameIndex === 0 ? frames.length - 1 : frameIndex - 1;
   } else if (buttonIndex === 3) {
+    // Next button pressed
     frameIndex = (frameIndex + 1) % frames.length;
   } else if (buttonIndex === 2 && frameIndex !== -1) {
-    // Correctly handle redirection without leading slash
-    const targetFrame = frames[frameIndex];
-    return res.redirect(302, targetFrame.url);
+    // Share button pressed
+    const shareText = encodeURIComponent(frames[frameIndex].sharetext);
+    const shareLink = `https://warpcast.com/~/compose?text=${shareText}&embeds[]=${encodeURIComponent(frames[frameIndex].url)}`;
+    return res.redirect(302, shareLink);  // Redirect to the share link
   }
 
   const currentFrame = frames[frameIndex] || frames[0];
@@ -28,7 +31,7 @@ export default async function handler(req, res) {
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="${imageUrl}" />
         <meta property="fc:frame:button:1" content="Previous" />
-        <meta property="fc:frame:button:2" content="Enter ${currentFrame.name}" />
+        <meta property="fc:frame:button:2" content="Share" />
         <meta property="fc:frame:button:3" content="Next" />
         <meta property="fc:frame:post_url" content="/api/frame" />
         <meta property="fc:frame:state" content="${frameIndex}" />
--- utils/frameData.js
+++ utils/frameData.js
@@ -2,70 +2,83 @@ export const frames = [
   {
     name: "Empower Goal",
     img: "empower.png",  
+    sharetext: "Set your public goal with the /empower frame by @aaronv.eth",
     url: "https://empower-goal-tracker.vercel.app"
   },
   {
     name: "War - Card Game",
     img: "cardwar.png",
+    sharetext: "Play the classic card game War!\n\nframe by @aaronv.eth",
     url: "https://war-of-cards-seven.vercel.app",
     initialState: { stage: 'initial' }
   },
   {
     name: "Guess Who Said It",
     img: "guessQuote.png",
+    sharetext: "Can you guess who said it?\n\nframe by @aaronv.eth",
     url: "https://guess-the-quote-mauve.vercel.app",
     initialState: { stage: 'initial' }
   },
   {
     name: "Plot Twist - Guess the Movie",
     img: "plotTwist.png",
+    sharetext: "How many movies can you get right?\n\nframe by @aaronv.eth",
     url: "https://plot-twist-nine.vercel.app",
     initialState: { stage: 'initial' }
   },
   {
     name: "This Day in History",
     img: "onthisday.png",
+    sharetext: "Check out what happened this day in history\n\nframe by @aaronv.eth",
     url: "https://time-capsule-jade.vercel.app",
     initialState: { stage: 'initial' }
   },
   {
     name: "Flowers Galore",
     img: "flower_flip_img.png",
+    sharetext: "Take a stroll through flowers galore!\n\nframe by @aaronv.eth",
     url: "https://flower-flip.vercel.app"
   },
   {
     name: "Dad Jokes",
     img: "dad_jokes.png",
+    sharetext: "get a laugh with some dad jokes!\n\nframe by @aaronv.eth",
     url: "https://dad-jokes-vert.vercel.app"
   },
   {
     name: "Your Daily Inspo",
     img: "zen.png",
+    sharetext: "Have you gotten your Daily Inspo yet?\n\nframe by @aaronv.eth",
     url: "https://daily-inspo.vercel.app"
   },
   {
     name: "Find Frens - Meme Channel",
     img: "success.png",
+    sharetext: "Find frens who're laughing at memes too!\n\nframe by @aaronv.eth",
     url: "https://find-meme-frens.vercel.app"
   },
   {
     name: "Trivia Game",
     img: "rivia.png",
+    sharetext: "How many trivia questions and you get right?\n\nframe by @aaronv.eth",
     url: "https://farcaster-trivia-one.vercel.app"
   },
   {
     name: "Fun Facts",
     img: "funfacts.png",
+    sharetext: "Here's some fun facts that may blow your mind.\n\nframe by @aaronv.eth",
     url: "https://funfacts-xi.vercel.app"
   },
   {
     name: "Find Frens - Success Channel",
     img: "success.png",
+    sharetext: "Success is your circle of frens!\n\nframe by @aaronv.eth",
     url: "https://success-omega.vercel.app"
   },
   {
     name: "Coindesk News",
     img: "coindeskrss.png",
+    sharetext: "Check on the latest Crypto News via Coindesk\n\nframe by @aaronv.eth",
     url: "https://coin-desk-news-frame.vercel.app"
   }
 ];
