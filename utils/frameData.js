f267287486b85387091822fc142e20b18b384637
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-10-03 17:38:15 -0400

    update

--- utils/frameData.js
+++ utils/frameData.js
@@ -3,102 +3,84 @@ export const frames = [
     name: "Empower Goal",
     img: "empower.png",  
     sharetext: "Set your public goal with the /empower frame by @aaronv.eth",
-    url: "https://empower-goal-tracker.vercel.app"
+    url: "https:empower-goal-tracker.vercel.app"
   },
   {
     name: "War - Card Game",
     img: "cardwar.png",
     sharetext: "Play the classic card game War!\n\nframe by @aaronv.eth",
-    url: "https://war-of-cards-seven.vercel.app"
+    url: "https:war-of-cards-seven.vercel.app"
   },
   {
     name: "Guess Who Said It",
     img: "guessQuote.png",
     sharetext: "Can you guess who said it?\n\nframe by @aaronv.eth",
-    url: "https://guess-the-quote-mauve.vercel.app"
+    url: "https:guess-the-quote-mauve.vercel.app"
   },
   {
     name: "Plot Twist - Guess the Movie",
     img: "plotTwist.png",
     sharetext: "How many movies can you get right?\n\nframe by @aaronv.eth",
-    url: "https://plot-twist-nine.vercel.app"
+    url: "https:plot-twist-nine.vercel.app"
   },
   {
     name: "This Day in History",
     img: "onthisday.png",
     sharetext: "Check out what happened this day in history\n\nframe by @aaronv.eth",
-    url: "https://time-capsule-jade.vercel.app"
+    url: "https:time-capsule-jade.vercel.app"
   },
   {
     name: "Flowers Galore",
     img: "flower_flip_img.png",
     sharetext: "Take a stroll through flowers galore!\n\nframe by @aaronv.eth",
-    url: "https://flower-flip.vercel.app"
+    url: "https:flower-flip.vercel.app"
   },
-  //{
-  //  name: "Sports Trivia",
-  //  img: "sportsGame.png",
-  //  sharetext: "Love sports?\n\nShow off your knowledge with Sports trivia.\n\nframe by @aaronv.eth",
-  //  url: "https://sports-trivia-zeta.vercel.app/"
-  //},
   {
     name: "Dad Jokes",
     img: "dad_jokes.png",
     sharetext: "get a laugh with some dad jokes!\n\nframe by @aaronv.eth",
-    url: "https://dad-jokes-vert.vercel.app"
+    url: "https:dad-jokes-vert.vercel.app"
   },
   {
     name: "Daily Inspo",
     img: "zen.png",
     sharetext: "Have you gotten your Daily Inspo yet?\n\nframe by @aaronv.eth",
-    url: "https://daily-inspo.vercel.app"
+    url: "https:daily-inspo.vercel.app"
   },
   {
     name: "Find Frens - Meme Channel",
     img: "success.png",
     sharetext: "Find frens who're laughing at memes too!\n\nframe by @aaronv.eth",
-    url: "https://find-meme-frens.vercel.app"
+    url: "https:find-meme-frens.vercel.app"
   },
   {
     name: "Trivia Game",
     img: "trivia.png",
     sharetext: "How many trivia questions and you get right?\n\nframe by @aaronv.eth",
-    url: "https://farcaster-trivia-one.vercel.app"
+    url: "https:farcaster-trivia-one.vercel.app"
   },
   {
     name: "Fun Facts",
     img: "funfacts.png",
     sharetext: "Here's some fun facts that may blow your mind.\n\nframe by @aaronv.eth",
-    url: "https://funfacts-xi.vercel.app"
+    url: "https:funfacts-xi.vercel.app"
   },
   {
     name: "Find Frens - Success Channel",
     img: "success.png",
     sharetext: "Success is your circle of frens!\n\nframe by @aaronv.eth",
-    url: "https://success-omega.vercel.app"
+    url: "https:success-omega.vercel.app"
   },
   {
     name: "Coindesk News",
     img: "coindeskrss.png",
     sharetext: "Check on the latest Crypto News via Coindesk\n\nframe by @aaronv.eth",
-    url: "https://coin-desk-news-frame.vercel.app"
+    url: "https:coin-desk-news-frame.vercel.app"
   },
   {
     name: "Guess the Anime Character",
     img: "animeGame.png",
     sharetext: "How many anime characters do you really know!\n\nframe by @aaronv.eth",
-    url: "https://animeguess.vercel.app"
-  },
-  //{
-  //  name: "Guess the Pokemon Character",
-  //  img: "pokeMain.png",
-  //  sharetext: "How many Pokemon characters do you really know!\n\nframe by @aaronv.eth",
-  //  url: "https://guess-pokemon-orpin.vercel.app"
-  //},
-  //{
-  //  name: "Guess what's in Space",
-  //  img: "spaceGame.png",
-  //  sharetext: "Do you really know what's out in space?!\n\nframe by @aaronv.eth",
-  // url: "https://space-guessing-game.vercel.app/"
+    url: "https:animeguess.vercel.app"
   }
 ];
